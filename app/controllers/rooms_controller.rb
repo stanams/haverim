2a920c6685875e63f0460a003cd79204e562ea1e
Stan <stan.amsellem@gmail.com>
2016-06-03 12:24:03 +0200

    create and style new room form

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -45,7 +45,7 @@ class RoomsController < ApplicationController
     end
 
     def room_params
-      params.require(:room).permit(:home_type, :room_type; :accomodate,
+      params.require(:room).permit(:home_type, :room_type, :accomodate,
                                    :bed_room, :bath_room, :listing_name,
                                    :summary, :address, :is_tv, :is_kitchen,
                                    :is_air, :is_heating, :is_internet, :is_cutlery,
