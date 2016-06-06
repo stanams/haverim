63647b92467a48d152124835a8d99005eabd2d89
Stan <stan.amsellem@gmail.com>
2016-06-06 21:32:23 +0200

    make ajax  messaging

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -17,7 +17,9 @@ class MessagesController < ApplicationController
     @message = @conversation.messages.new(message_params)
     @messages = @conversation.messages.order("created_at DESC")
     if @message.save
-      redirect_to conversation_messages_path(@conversation)
+      respond_to do |format|
+        format.js
+      end
     end
   end
 
