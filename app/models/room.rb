1f9cf4b7f90122033d683371c3bb71fd352e72f1
Stan <stan.amsellem@gmail.com>
2016-06-06 22:38:49 +0200

    make mvc of reviews with proper writer/reader permissions

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,16 +2,20 @@ class Room < ActiveRecord::Base
   belongs_to :user
   has_many :photos
   has_many :reservations
+  has_many :reviews
 
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
 
   validates :home_type, :room_type, :accomodate, :bed_room, :bath_room,
             :listing_name, :summary, :address, :cacheroute, :is_cutlery, presence: true
-
   validates :listing_name, length: { maximum: 50 }
   validates :summary, length: { maximum: 500 }
   validates :cacheroute, length: { maximum: 600 }
   validates :price, numericality: { only_integer: true, greater_than: 5 }
+
+  def average_rating
+    reviews.count == 0 ? 0 : reviews.average(:star).round(2)
+  end
 
 end
